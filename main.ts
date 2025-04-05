d3fea67c56d984b4872d4d1e2799121540ffac8c
Matthias <naets_matthias@hotmail.com>
2025-04-05 22:01:02 +0200

    SplitViewOpener initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,57 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, WorkspaceLeaf, MarkdownView } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class SplitViewOpener extends Plugin {
+  private sourceLeaf: WorkspaceLeaf | null = null;
+  private readingLeaf: WorkspaceLeaf | null = null;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+  async onload() {
+    console.log('Split View Opener loaded!');
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    this.app.workspace.onLayoutReady(() => {
+      // this.setupLeaves();
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+      this.registerEvent(
+        this.app.workspace.on('file-open', (file: TFile | null) => {
+          if (file && file.extension === 'md') {
+            this.updateFileInBothLeaves(file);
+          };
+        })
+      )
+    });
+  };
 
-	async onload() {
-		await this.loadSettings();
+  setupLeaves() {
+    const { workspace } = this.app;
+    const mdLeaves = this.app.workspace.getLeavesOfType('markdown');
+    const emptyLeaves = this.app.workspace.getLeavesOfType('empty');
+    const leaves = [...mdLeaves, ...emptyLeaves];
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    if (leaves.length >= 2) {
+      this.sourceLeaf = leaves[0];
+      this.readingLeaf = leaves[1];
+    } else if (leaves.length === 1) {
+      this.sourceLeaf = leaves[0];
+      this.readingLeaf = workspace.createLeafBySplit(this.sourceLeaf, 'vertical');
+    } else {
+      this.sourceLeaf = workspace.getLeaf(false);
+      this.readingLeaf = workspace.createLeafBySplit(this.sourceLeaf, 'vertical');
+    };
+  };
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  async updateFileInBothLeaves(file: TFile) {
+    console.log('Updating file in both leaves:', file.path);
+    this.setupLeaves();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    if (!this.sourceLeaf || !this.readingLeaf) {
+      return;
+    };
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    await this.sourceLeaf.openFile(file, { active: false });
+    const sourceView = this.sourceLeaf.view as MarkdownView;
+    await sourceView.setState({ mode: 'source' }, { history: true });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+    await this.readingLeaf.openFile(file, { active: false });
+    const readingView = this.readingLeaf.view as MarkdownView;
+    await readingView.setState({ mode: 'preview' }, { history: true });
+  }
+};
